2fc75875165f0c4d7ebf07a0d95ada6f031322f5
duypham965 <duypham965@gmail.com>
2022-10-21 20:31:17 +0700

    change position section

--- src/components/pages/LandingPage/index.jsx
+++ src/components/pages/LandingPage/index.jsx
@@ -70,65 +70,7 @@ export class Home extends React.Component {
         <span className="sr-only">Loading...</span>
       </div>
     </div> */}
-        {/* <!-- Spinner End --> */}
 
-        {/* <!-- Header Start --> */}
-        <div className="container-fluid bg-dark p-0 mb-5">
-          <div className="row g-0 flex-column-reverse flex-lg-row">
-            <div className="col-lg-6 p-0 wow fadeIn" data-wow-delay="0.1s">
-              <div className="header-bg h-100 d-flex flex-column justify-content-center p-5">
-                <h1 className="display-4 text-light mb-5">
-                  Đại lễ phật thành đạo 2022 - PL. 2565
-                </h1>
-                <div className="d-flex align-items-center pt-4 animated slideInDown">
-                  {/* <a href="" className="btn btn-primary py-sm-3 px-3 px-sm-5 me-5"
-                >Read More</a
-              > */}
-                  <button
-                    type="button"
-                    className="btn-play"
-                    data-bs-toggle="modal"
-                    data-src="https://www.youtube.com/embed/l9R_L65ssE0"
-                    data-bs-target="#videoModal"
-                  >
-                    <span></span>
-                  </button>
-                  <h6 className="text-white m-0 ms-4 d-none d-sm-block">
-                    Xem video
-                  </h6>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-              <div className="owl-carousel header-carousel">
-                <div className="owl-carousel-item">
-                  <img
-                    className="img-fluid"
-                    src="zoofari/img/carousel-1.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="owl-carousel-item">
-                  <img
-                    className="img-fluid"
-                    src="zoofari/img/carousel-2.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="owl-carousel-item">
-                  <img
-                    className="img-fluid"
-                    src="zoofari/img/carousel-3.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <!-- Header End --> */}
-
-        {/* <!-- Video Modal Start --> */}
         <div
           className="modal modal-video fade"
           id="videoModal"
@@ -165,11 +107,67 @@ export class Home extends React.Component {
             </div>
           </div>
         </div>
-        {/* <!-- Video Modal End --> */}
-
         {/* <!-- About Start --> */}
         <CeremonyServingRegister submitTarget="/register-result" />
         {/* <!-- About End --> */}
+
+        <div className="container-xxl p-0 mb-5">
+          <div className="row g-0 mx-auto">
+            <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 p-0 wow fadeIn" data-wow-delay="0.1s">
+              <div className="header-bg h-100 d-flex flex-column p-5">
+                <h1 className="display-4 text-light mb-5">
+                  Đại lễ phật thành đạo 2022 - PL. 2565
+                </h1>
+                <div className="d-flex align-items-center pt-4 animated slideInDown">
+                  {/* <a href="" className="btn btn-primary py-sm-3 px-3 px-sm-5 me-5"
+                >Read More</a
+              > */}
+                  <button
+                    type="button"
+                    className="btn-play"
+                    data-bs-toggle="modal"
+                    data-src="https://www.youtube.com/embed/l9R_L65ssE0"
+                    data-bs-target="#videoModal"
+                  >
+                    <span></span>
+                  </button>
+                  <h6 className="text-white m-0 ms-4 d-none d-sm-block">
+                    Xem video
+                  </h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 wow fadeIn" data-wow-delay="0.5s">
+              <div className="owl-carousel header-carousel">
+                <div className="owl-carousel-item">
+                  <img
+                    className="img-fluid"
+                    src="zoofari/img/carousel-1.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="owl-carousel-item">
+                  <img
+                    className="img-fluid"
+                    src="zoofari/img/carousel-2.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="owl-carousel-item">
+                  <img
+                    className="img-fluid"
+                    src="zoofari/img/carousel-3.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <!-- Header End --> */}
+
+        {/* <!-- Video Modal Start --> */}
+
 
         {/* <!-- Facts Start --> */}
         <div
